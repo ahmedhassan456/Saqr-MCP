88e6ef4165fd8ccc0c2ab85c89654dbdd0645261
Ahmed Saqr <ah8081189@gmail.com>
2025-05-08 13:21:55 +0300

    Add main.py with asynchronous client connection and chat loop functionality

---
+++ main.py
@@ -0,0 +1,22 @@
+from src.client import SaqrMCPClient
+import asyncio
+
+async def main():
+    client = SaqrMCPClient()
+    try:
+        await client.connect_to_server(
+            args=[
+                "run", 
+                "--with",
+                "mcp", 
+                "mcp", 
+                "run", 
+                "src\\server.py"
+            ]
+        )
+        await client.chat_loop()
+    finally:
+        await client.cleanup()
+
+if __name__ == "__main__":
+    asyncio.run(main())
