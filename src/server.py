76306aa4cea43a85956d96efc1be561ce197931c
Ahmed Saqr <ah8081189@gmail.com>
2025-06-01 03:46:52 +0300

    update the search tool and add word file generator tool

--- src/server.py
+++ src/server.py
@@ -4,6 +4,9 @@ from logger import logger
 from tavily import TavilyClient
 import os
 from dotenv import load_dotenv
+from docx import Document
+import markdown
+from htmldocx import HtmlToDocx
 
 _ = load_dotenv()
 
@@ -12,21 +15,53 @@ search_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
 
 @mcp.tool()
 def web_search(query: str):
-    """Use this to find current information about any query related pages using a search engine.
+    """
+    This tool performs real-time web searches to retrieve up-to-date information from the internet
     
     Args:
-        query (str): The search query.
+        - query (str): The search query.
+    Return:
+        - A list of search results
     """
 
     try:
         results = search_client.search(query)
         if results:
-            return results
+            return results["results"]
         else:
             return "No results found."
     except Exception as e:
         logger.error(f"Error during web search: {e}")
         return "An error occurred while performing the search."
+
+@mcp.tool()
+def word_file_generator(filename: str, title: str, content: str):
+    """
+    This tool generates Microsoft Word (.docx) documents based on structured input
+
+    Args:
+        - filename (str): The name of the Word file to be saved (e.g., 'output.docx').
+        - title (str): The title to be added as a heading.
+        - content (str): The main text content as a Markdown string.
+    Return:
+        - A .docx file saved locally
+    """
+
+    try:
+        html = markdown.markdown(content, extensions=['extra', 'tables'])
+
+        doc = Document()
+        doc.add_heading(title, level=0)
+
+        new_parser = HtmlToDocx()
+        
+        new_parser.add_html_to_document(html, doc)
+        doc.save(filename)
+
+        return f"The word file created successfully with name: {filename}"
+    except Exception as e:
+        logger.error(f"Error creating Word file: {e}")
+        return f"An error occurred while creating the Word file: {e}"
 
 
 def main():
